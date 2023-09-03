0ff65470c8e537aae3cbcd198cd54dbc58499313
Miziev <mizzzev@outlook.com>
2023-09-03 16:25:12 +0300

    - added jwt dependency in pom.xml - created JwtTokenProvider class with all methods

---
+++ src/main/java/com/api/security/JwtTokenProvider.java
@@ -0,0 +1,60 @@
+package com.api.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app.jet-expiration-milliseconds}")
+    private long jwtExpiration;
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+
+        Date currentDate = new Date();
+        Date expDate = new Date(currentDate.getTime() + jwtExpiration);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expDate)
+                .signWith(key())
+                .compact();
+
+        return token;
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        String username = claims.getSubject();
+
+        return username;
+    }
+
+    public boolean validateToken(String token) {
+        Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parse(token);
+        return true;
+    }
+}
