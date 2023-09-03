de69d223152ef8dd89d02d2d415241ac921f677f
Miziev <mizzzev@outlook.com>
2023-09-03 14:20:26 +0300

    - created controller class for authentication with one method (register)

---
+++ src/main/java/com/api/controllers/AuthController.java
@@ -0,0 +1,27 @@
+package com.api.controllers;
+
+import com.api.dto.RegisterDto;
+import com.api.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin("*")
+public class AuthController {
+
+    private final AuthService authService;
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
+        String response = authService.register(registerDto);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
