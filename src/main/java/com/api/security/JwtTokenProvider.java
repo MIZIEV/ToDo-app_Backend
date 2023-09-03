a147efd4598916c94acd00ab6a68bb48622e4ecf
Miziev <mizzzev@outlook.com>
2023-09-03 16:32:03 +0300

    - created JwtAuthenticationEntryPoint class - added component annotation to JwtTokenProvider class

--- src/main/java/com/api/security/JwtTokenProvider.java
+++ src/main/java/com/api/security/JwtTokenProvider.java
@@ -6,10 +6,12 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 
+@Component
 public class JwtTokenProvider {
 
     @Value("${app.jwt-secret}")
