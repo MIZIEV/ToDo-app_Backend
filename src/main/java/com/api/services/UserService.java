363da4a9d27d8158a2a012e5a7b8f2ed65069828
Miziev <mizzzev@outlook.com>
2023-10-10 17:27:15 +0300

    - created functional for updating user dta & delete user profile

--- src/main/java/com/api/services/UserService.java
+++ src/main/java/com/api/services/UserService.java
@@ -4,4 +4,6 @@ import com.api.model.User;
 
 public interface UserService {
     User getUserByUsername(String username);
+    void updateUser(User editedUser,String username);
+    void deleteUser(String username);
 }
