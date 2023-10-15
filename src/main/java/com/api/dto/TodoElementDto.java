5b44c82ecb178952f989f4e60901e56043ff7103
Miziev <mizzzev@outlook.com>
2023-10-15 19:48:15 +0300

    - added override main methods (equals,hashcode,toString) for three classes loginDto,RegisterDto,TodoElementDto

--- src/main/java/com/api/dto/TodoElementDto.java
+++ src/main/java/com/api/dto/TodoElementDto.java
@@ -1,12 +1,15 @@
 package com.api.dto;
 
+import java.util.Objects;
+
 public class TodoElementDto {
 
     private Long id;
     private String elementName;
     private boolean isCompleted;
 
-    public TodoElementDto() {}
+    public TodoElementDto() {
+    }
 
     public TodoElementDto(Long id, String elementName, boolean isCompleted) {
         this.id = id;
@@ -36,5 +39,23 @@ public class TodoElementDto {
 
     public void setCompleted(boolean completed) {
         isCompleted = completed;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, elementName, isCompleted);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        TodoElementDto myClass = (TodoElementDto) obj;
+        return Objects.equals(id, myClass.id) && Objects.equals(elementName, myClass.elementName) && isCompleted == myClass.isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return id + ")" + elementName + ", " + isCompleted;
     }
 }
