c3c67ced65299c0c2d996904b293d95050384b38
Olexandr Miziev <mizzzev@outlook.com>
2024-03-23 14:21:49 +0200

    - removed unnecessary code from todo service

--- src/main/java/com/api/services/TodoService.java
+++ src/main/java/com/api/services/TodoService.java
@@ -2,15 +2,11 @@ package com.api.services;
 
 import com.api.model.Todo;
 
-import java.util.List;
-
 public interface TodoService {
 
     void saveTodo(Todo todo);
 
     Todo getTodoById(Long id);
-
-    List<Todo> elementsList(String todoUniqueKey);
 
     void changeCompleteStatus(Long id);
 
--- src/main/java/com/api/services/impl/TodoServiceImpl.java
+++ src/main/java/com/api/services/impl/TodoServiceImpl.java
@@ -2,38 +2,26 @@ package com.api.services.impl;
 
 import com.api.model.Todo;
 import com.api.repositories.TodoRepository;
-import com.api.repositories.TaskRepository;
 import com.api.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 public class TodoServiceImpl implements TodoService {
 
     private final TodoRepository elementRepository;
-    private final TaskRepository taskRepository;
 
     @Autowired
-    public TodoServiceImpl(TodoRepository elementRepository, TaskRepository taskRepository) {
+    public TodoServiceImpl(TodoRepository elementRepository) {
         this.elementRepository = elementRepository;
-        this.taskRepository = taskRepository;
     }
 
     @Override
     @Transactional(readOnly = false)
     public void saveTodo(Todo todo) {
         elementRepository.save(todo);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public List<Todo> elementsList(String taskUniqueKey) {
-        //List<Todo> elements = taskRepository.findByTaskUniqueKey(taskUniqueKey).getTodoList();
-        return null;
     }
 
     @Override
