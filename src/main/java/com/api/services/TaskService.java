b99aba8c428efae941951e0cfc8b2fe7024942a1
Miziev <mizzzev@outlook.com>
2024-04-10 21:08:56 +0300

    - refactored getCompleted/InCompletedTasks methods - created custom method in repository & implemented it on service & controller

--- src/main/java/com/api/services/TaskService.java
+++ src/main/java/com/api/services/TaskService.java
@@ -2,12 +2,24 @@ package com.api.services;
 
 import com.api.model.Task;
 
+import java.util.List;
+
 public interface TaskService {
     void saveNewTask(Task task);
+
+    List<Task> getCompletedTasks(String username);
+
+    List<Task> getInCompletedTasks(String username);
+
     void updateTask(Task editedTask, Long id);
+
     void changeCompletedStatus(Long id);
+
     void deleteTask(Long id);
+
     void deleteAllTasks();
+
     void deleteCompletedTasks();
+
     Task getTaskById(Long id);
 }
